051db419bc695c23c334e6b980bb661326720abe
whp <whp98@outlook.com>
2020-03-06 17:45:43 +0800

    完成第二章作业

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,13 +1,5 @@
 //index.js
 
 Page({
-  data: {
-    imgSrc:"/images/icons8_cat_128px.png"
-  },
-  //事件处理函数
-  tapCat:function(){
-    let audio=wx.createInnerAudioContext();
-    audio.src ='/audios/10310.wav';
-    audio.play();
-  }
+  
 })
