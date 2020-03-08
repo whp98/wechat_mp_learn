05438e1ec9c57c44864a4808879547d5cfb8b231
whp <whp98@outlook.com>
2020-03-09 01:00:30 +0800

    函数和条件语句

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,14 +1,26 @@
 //index.js
 
 Page({
-    // 摄氏温度转换华氏温度
-    calce:function(e){
-        // 定义变量
-        var C,F; 
-        // 获取input组件的输入的数值
-        C = e.detail.value;
+    // 函数逻辑转换
+    calc:function(e){
+        // 定义变量x，y
+        var x,y; 
+        // 获取input组件的输入的数值并且赋值
+        x = e.detail.value;
+
+        //根据x值进行判断求y值
+        if(x<0){
+            y=Math.abs(x);
+        }else if(x<10){
+            y=Math.exp(x)*Math.sin(x);
+        }else if(x<20){
+            y=Math.pow(x,3);
+        }else{
+            y = (3+2*x)*Math.log(x);
+        }
+
         this.setData({
-            F: C*9/5+32
+            y: y
         })
     }
 })
