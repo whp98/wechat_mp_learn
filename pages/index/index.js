290eb8c35a773c0b0e8b2faf9392364979c4bad0
whp <whp98@outlook.com>
2020-03-10 04:18:33 +0800

    完成变色的三色旗

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,48 +1,34 @@
 //index.js
-// 计时器显示的数字
-var num=60;
-// 计时器ID
-var timerID;
+
 
 Page({
-    data:{
-        hidden:true,
-        num:num
-    },
-    onLoad: function(options){
-        var that = this;
-        setTimeout(()=>{
-            that.show()
-        },500)//500ms后显示计时界面
-    },
-    show: function(){
-        var that = this;
-        that.setData({
-            hidden:false//显示计时界面
+    // 创建颜色
+    createColor:function(){
+        var color = [];//数组
+        // 十六进制字符集
+        var letters = "0123456789ABCDEF";
+        for(var i=0;i<3;i++){
+            var c = "#";
+            for(var j=0;j<6;j++){
+                c += letters[Math.floor(Math.random()*16)];
+            }
+            color.push(c);
+        }
+        console.log(color);
+        this.setData({
+            color1:color.pop(),
+            color2:color.pop(),
+            color3:color.pop()
         })
     },
-    start: function(){
-        var that = this;
-        timerID=setInterval(()=>{
-            that.timer()
-        },1000)//每隔一秒调用一次timer函数
+    onLoad: function(e){
+        this.createColor();
+        setInterval(()=>{
+            this.createColor();
+        },5000)
     },
-    stop:function(){
-        clearInterval(timerID)//清除计时器
-    },
-
-    timer:function(){
-        var that = this;
-        console.log(num)
-        if(num>0){
-            that.setData({
-                num:num--
-            })
-        }else{
-            that.setData({
-                num:0
-            })
-        }
-        console.log(num)
+    changeColor:function(e){
+        this.createColor();
     }
+
 })
