39e5edb1b669e3ba6623ab0bf7d8d030676399b2
whp <whp98@outlook.com>
2020-03-09 14:19:40 +0800

    完成随机数求和

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,25 +1,36 @@
 //index.js
 //三个全局变量
-var start,end,sum;
+var rand,sum;
+//产生随机数
+function createRand(){
+    //初始化随机数数组
+    rand=[];
+    //初始化sum变量
+    sum=0;
+    for(var i=0;i<6;i++){
+        var r = (Math.random()*100).toFixed(2)*1;//产生100以内的随机数
+        //将产生的随机数放到数组中
+        rand.push(r);
+        sum += rand[i];
+        console.log(rand[i]);
+    }
+    console.log(sum);
+}
 Page({
-    startNum: function(e){
-        //将input组件的value转换为整数并赋值
-        start = parseInt(e.detail.value);
-        console.log(start);
-    },
-    endNum: function(e){
-        end = parseInt(e.detail.value);
-        console.log(end);
-    },
-
-    calc: function(){
-        sum=0;
-        for(var i = start ; i <= end ; i++){
-            sum = sum+i;
-        }
-        console.log(sum);
+    onLoad: function(){
+        //产生随机数
+        createRand();
         this.setData({
-            sum: sum
+            rand:rand,
+            sum:sum
+        })
+    },
+    newRand:function(){
+        //产生随机数
+        createRand();
+        this.setData({
+            rand:rand,
+            sum:sum
         })
     }
 })
