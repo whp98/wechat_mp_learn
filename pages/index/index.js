852cafff06e1ebf0ad4342366ae152d682f060e5
whp <whp98@outlook.com>
2020-03-11 00:38:47 +0800

    完成条件渲染的案例

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,23 +1,9 @@
 //index.js
-const app = getApp();
-var util = require('../utils/util.js');
-var indexMsg = '我是来自index.js的变量';
-function indexFunc(){
-    return '我是来自index.js的函数';
-}
-
 Page({
-
+    //渲染数据参考
     data: {
-        /*全局*/
-        msg1: app.globalMsg,
-        msg2: app.globalFunc(),
-        /*本模块*/
-        msg3: indexMsg,
-        msg4: indexFunc(),
-        /*util模块*/
-        msg5: util.utilMsg,
-        msg6: util.utilFunc()
+        color:'pink',
+        length:15
     }
 
 })
