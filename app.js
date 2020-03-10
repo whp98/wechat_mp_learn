6b21d4bdac7a58ba3e0685c940fa7665a47d6b6b
whp <whp98@outlook.com>
2020-03-11 01:33:16 +0800

    作业四完成

--- app.js
+++ app.js
@@ -1,7 +1,4 @@
 //app.js
 App({
-    globalMsg:'来自App.js的全局变量',
-    globalFunc:function(){
-        return "来自App.js的全局函数"
-    }
+    
 })
--- pages/index/index.js
+++ pages/index/index.js
@@ -1,9 +1,4 @@
 //index.js
 Page({
-    //渲染数据参考
-    data: {
-        color:'pink',
-        length:15
-    }
-
+    
 })
