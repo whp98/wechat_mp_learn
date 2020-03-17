02b68fa74836866fdee2f4bfbc4f9b38c153f08b
whp <whp98@outlook.com>
2020-03-17 20:23:37 +0800

    三角形计算器

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,27 +1,33 @@
 //index.js
-var C;
 Page({
     formSubmit: function(e) {
-        C = parseInt(e.detail.value.cels);
-        console.log(C);
+        var a = parseFloat(e.detail.value.a);
+        var b = parseFloat(e.detail.value.b);
+        var c = parseFloat(e.detail.value.c);
+        var area;
+        if(a+b<=c||a+c<=b||b+c<=a){
+            wx.showToast({
+                title: '三角形的两边之和小于第三条边',
+                icon:'none',
+                duration:2500
+            });
+            this.formReset();
+            return;
+        }else{
+            var s = (a+b+c)/2;
+            area = Math.sqrt(s*(s-a)*(s-b)*(s-c))
+        }
         this.setData({
-            MY: (C / 6.8801).toFixed(4),
-            YB: (C / 8.7873).toFixed(4),
-            XG: (C / 0.8805).toFixed(4),
-            OY: (C / 7.8234).toFixed(4),
-            HY: (C / 0.0061).toFixed(4),
-            RY: (C / 0.0610).toFixed(4)
-        })
+            result:area
+        });
+
     },
     formReset: function(e) {
         this.setData ({
-            MY: '',
-            YB: '',
-            XG: '',
-            OY: '',
-            HY: '',
-            MY: '',
-            RY: '',
+            a:'',
+            b:'',
+            c:'',
+            result:''
         })
     }
 })
