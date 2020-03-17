fcefd01175b24e12f0eb2fe118f97505d4148dd8
whp <whp98@outlook.com>
2020-03-17 19:38:17 +0800

    完成汇率计算器

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,25 +1,27 @@
 //index.js
+var C;
 Page({
-    data:{
-        stu01:{
-            name:"www",
-            age:12,
-            gender:"man"
-        },
-        stu02: {
-            name: "小明",
-            age: 122,
-            gender: "女"
-        },
-        stu03: {
-            name: "sss",
-            age: 32,
-            gender: "男"
-        }
-    },
-    scoreInput:function(e){
+    formSubmit: function(e) {
+        C = parseInt(e.detail.value.cels);
+        console.log(C);
         this.setData({
-            score:e.detail.value
+            MY: (C / 6.8801).toFixed(4),
+            YB: (C / 8.7873).toFixed(4),
+            XG: (C / 0.8805).toFixed(4),
+            OY: (C / 7.8234).toFixed(4),
+            HY: (C / 0.0061).toFixed(4),
+            RY: (C / 0.0610).toFixed(4)
+        })
+    },
+    formReset: function(e) {
+        this.setData ({
+            MY: '',
+            YB: '',
+            XG: '',
+            OY: '',
+            HY: '',
+            MY: '',
+            RY: '',
         })
     }
 })
