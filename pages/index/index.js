a57f76573ff8c4e051f55ed24ebd42b0843a29ec
whp <whp98@outlook.com>
2020-04-07 22:28:10 +0800

    完成7.1案例学习了picker的使用

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,32 +1,61 @@
 //index.js
+function Person(name,sex,birthPlace,birthDay,height,weight){
+    //定义构造函数，用于创建对象
+    this.name =name;
+    this.sex=sex;
+    this.birthPlace=birthPlace;
+    this.birthDay=birthDay;
+    this.height=height;
+    this.weight=weight;
+}
+
 Page({
     data:{
-        background:['bc-red','bc-green','bc-blue'],
-        indicatorDots:true,
-        autoplay:false,
-        circular:false,
-        vertical:false,
-        interval:2000,
-        duration:500
+        flag:true,
+        gender:['男','女']
     },
-    changeIndicatorDots:function(e){
+
+    nameInput:function(e){
+        this.name=e.detail.value
+    },
+    //选择性别之后把结果放到页面上
+    pickerSex:function(e){
+        this.sex=this.data.gender[e.detail.value]
         this.setData({
-            indicatorDots:!this.data.indicatorDots
+            sex:this.sex
         })
     },
-    changeAutoplay:function(e){
+    //将籍贯结果显示
+    pickerRegion:function(e){
+        this.birthPlace=e.detail.value;
         this.setData({
-            autoplay: !this.data.autoplay
+            birthPlace:this.birthPlace
         })
     },
-    changeCircular: function (e) {
+    //出生日期选择器
+    pickerDate:function(e){
+        this.birthDay=e.detail.value;
         this.setData({
-            circular: !this.data.circular
+            birthDay:this.birthDay
         })
     },
-    changeVertical: function (e) {
+    heightInput:function(e){
+        this.height=e.detail.value
         this.setData({
-            vertical: !this.data.vertical
+            height:this.height
+        })
+    },
+    weightInput:function(e){
+        this.weight = e.detail.value
+        this.setData({
+            height: this.weight
+        })
+    },
+    showMessage:function(e){
+        var p = new Person(this.name,this.sex,this.birthPlace,this.birthDay,this.height,this.weight)
+        this.setData({
+            flag:false,
+            person:p
         })
     }
 })
