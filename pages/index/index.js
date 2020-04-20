e44a9dddd0b4088badb41b95d7d3b4cc1e4168f6
whp <whp98@outlook.com>
2020-04-21 05:22:45 +0800

    完成视频播放和随机颜色弹幕的案例

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,26 +1,45 @@
 //index.js
+
+function getRandomColor(){
+    let rgb=[]
+    for(let i=0;i<3;i++){
+        let color = Math.floor(Math.random()*256).toString(16)
+        color=color.length==1 ? '0' + color : color
+        rgb.push(color)
+    }
+    return "#"+rgb.join('')
+}
+
 Page({
-    data: {
-        poster:'https://y.gtimg.cn/music/photo_new/T002R300x300M000001hGx1Z0so1YX.jpg?max_age=2592000',
-        name:'我是如此相信',
-        author:'周杰伦',
-        src:'http://isure.stream.qqmusic.qq.com/C400001PLl3C4gPSCI.m4a?guid=4486890145&vkey=B7CC4E7890585EEE272DAC578ADC65BC6AA32E2564BF91EE20924FBAAA994F96CAF590882907514D46EF98BCED45217D381CBFB2E417770D&uin=208&fromtag=66',
-    
+    data:{
+        danmulist:[
+            {
+                text:"第一条弹幕",
+                color:'#568910',
+                time:1
+            },
+            {
+                text: "第二条弹幕",
+                color: '#ff8910',
+                time: 3
+            },
+            {
+                text: "第三条弹幕",
+                color: '#128910',
+                time: 4
+            }
+        ]
     },
     onLoad:function(options){
-        //页面初始化
-        this.audioCtx = wx.createAudioContext('myAudio')//创建音频上下文
+        this.videoCtx = wx.createVideoContext("myVideo")
     },
-    audioPlay:function(){
-        this.audioCtx.play()
+    inputBlur:function(e){
+        this.inputValue=e.detail.value;
     },
-    audioPause:function(){
-        this.audioCtx.pause()
-    },
-    audioStart:function(){
-        this.audioCtx.seek(0)
-    },
-    audio14:function(){
-        this.audioCtx.seek(14)
+    sendDanmu:function(){
+        this.videoCtx.sendDanmu({
+            text:this.inputValue,
+            color:getRandomColor()
+        })
     }
 })
