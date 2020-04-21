95d916e6b29372fade884f4cef97618bc606f10c
whp <whp98@outlook.com>
2020-04-21 13:36:10 +0800

    完成案例考试时间选择

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,45 +1,47 @@
 //index.js
 
-function getRandomColor(){
-    let rgb=[]
-    for(let i=0;i<3;i++){
-        let color = Math.floor(Math.random()*256).toString(16)
-        color=color.length==1 ? '0' + color : color
-        rgb.push(color)
-    }
-    return "#"+rgb.join('')
-}
-
 Page({
-    data:{
-        danmulist:[
-            {
-                text:"第一条弹幕",
-                color:'#568910',
-                time:1
-            },
-            {
-                text: "第二条弹幕",
-                color: '#ff8910',
-                time: 3
-            },
-            {
-                text: "第三条弹幕",
-                color: '#128910',
-                time: 4
-            }
-        ]
+    data: {
+        getEmail: '',
+        getPwd: '',
+        getPwdConfirm: ''
     },
-    onLoad:function(options){
-        this.videoCtx = wx.createVideoContext("myVideo")
+    formSubmit: function (e) { //提交表单（点击注册按钮）事件
+        if (e.detail.value.email.length == 0 || e.detail.value.password.length == 0) { //判断邮箱和密码输入框内容是否为空。
+            this.setData({
+                showMsg01: '邮箱或密码不得为空！',
+            })
+        } else if (e.detail.value.password != e.detail.value.confirm) { //判断密码和确认密码是否一致
+            this.setData({
+                showMsg02: '两次输入密码不一致！',
+                getPwd: '', //清空输入框内容
+                getPwdConfirm: ''
+            })
+        } else {
+            console.log("aaaa")
+            wx.navigateTo({ //页面跳转
+                url: '../details/details',
+            })
+        }
     },
-    inputBlur:function(e){
-        this.inputValue=e.detail.value;
+    inputemail: function (e) { //input组件事件函数
+        var email = e.detail.value
+        var checkedNum = this.checkEmail(email)
     },
-    sendDanmu:function(){
-        this.videoCtx.sendDanmu({
-            text:this.inputValue,
-            color:getRandomColor()
-        })
+
+    checkEmail: function (email) { //自定义函数，检查输入的邮箱地址是否满足要求
+        let str = /^[a-zA-Z0-9_.-]+@[a-zA-Z0-9-]+(\.[a-zA-Z0-9-]+)*\.[a-zA-Z0-9]{2,6}$/ //正则表达式
+        if (str.test(email)) { //检查正则表达式是否符合邮箱地址要求
+            return true
+        } else {
+            wx.showToast({ //显示消息提示框
+                title: '邮箱格式错误',
+                icon: 'loading'
+            })
+            this.setData({
+                getEmail: ''
+            })
+            return false
+        }
     }
 })
