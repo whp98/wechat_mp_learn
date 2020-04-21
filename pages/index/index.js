ab255f434bf360868521fa8f62ccd838fe786045
whp <whp98@outlook.com>
2020-04-21 14:47:24 +0800

    完成变脸

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,47 +1,45 @@
 //index.js
 
-Page({
-    data: {
-        getEmail: '',
-        getPwd: '',
-        getPwdConfirm: ''
-    },
-    formSubmit: function (e) { //提交表单（点击注册按钮）事件
-        if (e.detail.value.email.length == 0 || e.detail.value.password.length == 0) { //判断邮箱和密码输入框内容是否为空。
-            this.setData({
-                showMsg01: '邮箱或密码不得为空！',
-            })
-        } else if (e.detail.value.password != e.detail.value.confirm) { //判断密码和确认密码是否一致
-            this.setData({
-                showMsg02: '两次输入密码不一致！',
-                getPwd: '', //清空输入框内容
-                getPwdConfirm: ''
-            })
-        } else {
-            console.log("aaaa")
-            wx.navigateTo({ //页面跳转
-                url: '../details/details',
-            })
-        }
-    },
-    inputemail: function (e) { //input组件事件函数
-        var email = e.detail.value
-        var checkedNum = this.checkEmail(email)
-    },
+function getRandIn(){
+    //产生0-9之间的随机整数
+    return Math.floor(Math.random()*10);
+}
 
-    checkEmail: function (email) { //自定义函数，检查输入的邮箱地址是否满足要求
-        let str = /^[a-zA-Z0-9_.-]+@[a-zA-Z0-9-]+(\.[a-zA-Z0-9-]+)*\.[a-zA-Z0-9]{2,6}$/ //正则表达式
-        if (str.test(email)) { //检查正则表达式是否符合邮箱地址要求
-            return true
-        } else {
-            wx.showToast({ //显示消息提示框
-                title: '邮箱格式错误',
-                icon: 'loading'
-            })
-            this.setData({
-                getEmail: ''
-            })
-            return false
-        }
+Page({
+    
+    data:{
+        index: 0,//初始化数组下标为0
+        imgArr: [
+            '/images/1.jpeg',
+            '/images/2.jpg',
+            '/images/3.jpeg',
+            '/images/4.jpg',
+            '/images/5.jpeg',
+            '/images/6.jpg',
+            '/images/7.jpeg',
+            '/images/8.jpeg',
+            '/images/9.jpeg',
+            '/images/3.jpg'
+        ],
+    },
+    changeFace:function(){
+        this.setData({
+            index:getRandIn()
+        })
+    },
+    onShow:function(){
+        var that = this;
+        wx.onAccelerometerChange(function(res){//加速度变化监听函数
+            if(res.x> 0.5|| res.y>0.5 || res.z>0.5){
+                wx.showToast({
+                    title: '摇一摇成功',
+                    icon:'success',
+                    duration:1000 //显示时间
+                })
+                that.changeFace()
+            }
+        })
     }
+    
+
 })
