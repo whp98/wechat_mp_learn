c99d19eed0d7f44d3c23547798cfc82027f7a97e
whp <whp98@outlook.com>
2020-05-11 11:15:29 +0800

    完成基本绘图操作

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,43 +1,100 @@
 //index.js
-
-
+//创建画布上下文
+var ctx = wx.createCanvasContext('myCanvas')
 Page({
-    getInput: function(e) {
-        this.inputVal = e.detail.value;
+    clear:function(){
+        ctx.draw() //刷新屏幕 显示绘制效果
     },
-
-    onShow: function() {
-        var that = this;
-        that.isShow = true;
-        //加速度改变的api
-        wx.onAccelerometerChange(function(e) {
-            if (!that.isShow) {
-                //判断小程序是否显示
-                return
-            }
-            //判断手机是否晃动到某一程度
-            if (e.x > 0.5 || e.y > 0.5 || e.z > 0.5) {
-                wx.showToast({ //显示消息框
-                    title: '摇一摇成功',
-                    icon: 'success',
-                    duration: 2000
-                })
-                var result = 1;
-                console.log(that.inputVal)
-                for (var i = 1; i <= that.inputVal; i++) {
-                    result = result * i;
-                    console.log(result)
-                }
-                console.log(result)
-                that.setData({
-                    result:result
-                })
-            }
-        })
-
+    drawDot:function(e){
+        //参数 中心坐标 半径 开始角度 结束角度
+        ctx.arc(200,200,20,0,2*Math.PI)//绘制图形
+        ctx.setFillStyle('black') //设置填充样式
+        ctx.fill() //填充
+        ctx.draw()
     },
-    onHide: function() {
-        this.isShow = false
+    drawCircle: function(){
+        ctx.setFillStyle('black')
+        ctx.arc(200,200,10,0,2*Math.PI)
+        ctx.fill() //填充
+
+        ctx.setStrokeStyle('red') //描边颜色
+        ctx.moveTo(300,200)
+        ctx.arc(200,200,100,0,2*Math.PI)
+        ctx.stroke()
+        ctx.draw()
+    },
+    //画虚线
+    drawDash:function(){
+        ctx.setStrokeStyle("red")
+        ctx.setLineDash([20,10])//虚线样式
+        ctx.setLineWidth(10)
+        ctx.moveTo(50,100)
+        ctx.lineTo(250,100)
+        ctx.lineTo(150,300)
+        ctx.lineTo(50,100)
+        ctx.stroke() //画出路径的边框
+        ctx.draw()
+        ctx.setLineDash([0,0]) //将线变成原来的样子
+        ctx.setLineWidth(1)
+    },
+    //设置线连接，和端点
+    capAndJoin:function(){
+        //设置线条颜色宽度还有线条连接样式
+        ctx.setStrokeStyle('red')
+        ctx.setLineWidth(20)
+        //圆形端点
+        ctx.setLineCap('round')
+        //尖角连接
+        ctx.setLineJoin('miter')
+        ctx.moveTo(50,50)
+        ctx.lineTo(250,50)
+        ctx.lineTo(50,250)
+        ctx.lineTo(250,250)
+        ctx.stroke()
+        ctx.draw()
+        //恢复原来的样式
+        ctx.setLineWidth(1)
+        ctx.setLineCap('butt')
+        ctx.setLineJoin('mitter')
+    },
+    //字体绘制
+    drawText:function(){
+        ctx.setFillStyle("red")
+        ctx.setFontSize(40)//设置字体大小
+        ctx.setTextBaseline('bottom')
+        ctx.fillText('HDU杭电',80,80)//绘制文本
+
+        ctx.setFillStyle('yellow')
+        ctx.setTextBaseline('top')
+        ctx.fillText('Hello World',80,80)
+
+        ctx.setFillStyle('black')
+        ctx.rotate(30*Math.PI/180)//旋转字体
+        ctx.fillText("你好",150,80)
+        ctx.draw()
+    },
+    //绘制圆形渐变
+    circularGrad:function(){
+        var grd = ctx.createCircularGradient(175,175,125)//以175.175为圆心125为半径渐变
+        grd.addColorStop(0,'purple')//以渐变为起点
+        grd.addColorStop(1,'white')//添加渐变终点
+        ctx.setFillStyle(grd)
+        //起点50，50宽高都是250
+        ctx.fillRect(50,50,250,250)
+        ctx.draw()
+    },
+    //设置阴影矩形
+    shadowRect:function(){
+        ctx.setFillStyle('orange')
+        ctx.setShadow(20,20,50,'yellow')
+        ctx.fillRect(50,50,250,250)
+        ctx.draw()
+    },
+    translucent:function(){
+        ctx.setFillStyle('red')
+        ctx.setGlobalAlpha(0.2)//透明
+        ctx.fillRect(50,50,250,250)
+        ctx.draw()
+        ctx.setGlobalAlpha(1)//恢复设置
     }
-
 })
