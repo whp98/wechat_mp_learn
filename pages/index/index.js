71903ba7a1f2e1d23f4ebd167cfb4bf965b4ea8f
whp <whp98@outlook.com>
2020-05-12 13:45:32 +0800

    完成自由绘制

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,121 +1,75 @@
 //index.js
 //创建画布上下文
-var ctx = wx.createCanvasContext('myCanvas')
 Page({
-    //加载之后绘制
-    onShow:function(options){             
-        this.drawSin()
+    //清除
+    isClear:false,
+    data:{
+        pen:5,
+        color:'#000000'
     },
-    //绘制一个点
-    drawDotX:function(x,y){
-        ctx.arc(x ,y ,5 ,0 , 2*Math.PI)
-        ctx.setFillStyle('black')
-        ctx.fill()
-        ctx.draw(true)
+    onLoad:function(){
+        this.ctx =wx.createCanvasContext('myCanvas', this)//绘制画布绘图环境
     },
-    //描绘正弦曲线
-    drawSin:function(){
-        for(var x=0 ;x < 2*Math.PI; x += Math.PI/180){
-            var y=Math.sin(x);
-            this.drawDotX(30+x*30,80+y*30);
-            console.log(80 + y * 50)
+    touchStart:function(e){
+        this.x1 = e.changedTouches[0].x;
+        this.y1 = e.changedTouches[0].y;
+        if(this.isClear){
+            this.ctx.setStrokeStyle("#FFFFFF");
+            this.ctx.setLineCap("round");
+            this.ctx.setLineJoin('round');
+            this.ctx.setLineWidth(20);
+            this.ctx.beginPath();
+        }else{
+            this.ctx.setStrokeStyle(this.data.color);
+            this.ctx.setLineWidth(this.data.pen);
+            this.ctx.setLineCap('round');
+            this.ctx.beginPath();
         }
     },
+
+    touchMove:function(e){
+        var x2 = e.changedTouches[0].x;
+        var y2 = e.changedTouches[0].y;
+        if(this.isClear){
+            this.ctx.moveTo(this.x1,this.y1);
+            this.ctx.lineTo(x2,y2);
+            this.ctx.stroke();
+            this.x1=x2;
+            this.y1=y2;
+        }else{
+            this.ctx.moveTo(this.x1,this.y1);
+            this.ctx.lineTo(x2,y2);
+            this.ctx.stroke();
+            this.x1=x2;
+            this.y1=y2;
+        }
+        this.ctx.draw(true);
+    },
+    touchEnd:function(){},
+
+    penSelect:function(e){
+        console.log(222)
+        this.setData({
+            pen:e.currentTarget.dataset.param
+        });
+        this.isClear = false;
+    },
+    colorSelect: function (e) {
+        this.setData({
+            color: e.currentTarget.dataset.param
+        });
+        this.isClear = false;
+    },
     clear:function(){
-        ctx.draw() //刷新屏幕 显示绘制效果
+        this.isClear=true;
     },
-    drawDot:function(e){
-        //参数 中心坐标 半径 开始角度 结束角度
-        ctx.arc(200,200,20,0,2*Math.PI)//绘制图形
-        ctx.setFillStyle('black') //设置填充样式
-        ctx.fill() //填充
-        ctx.draw()
-    },
-    drawCircle: function(){
-        ctx.setFillStyle('black')
-        ctx.arc(200,200,10,0,2*Math.PI)
-        ctx.fill() //填充
 
-        ctx.setStrokeStyle('red') //描边颜色
-        ctx.moveTo(300,200)
-        ctx.arc(200,200,100,0,2*Math.PI)
-        ctx.stroke()
-        ctx.draw()
-    },
-    //画虚线
-    drawDash:function(){
-        ctx.setStrokeStyle("red")
-        ctx.setLineDash([20,10])//虚线样式
-        ctx.setLineWidth(10)
-        ctx.moveTo(50,100)
-        ctx.lineTo(250,100)
-        ctx.lineTo(150,300)
-        ctx.lineTo(50,100)
-        ctx.stroke() //画出路径的边框
-        ctx.draw()
-        ctx.setLineDash([0,0]) //将线变成原来的样子
-        ctx.setLineWidth(1)
-    },
-    //设置线连接，和端点
-    capAndJoin:function(){
-        //设置线条颜色宽度还有线条连接样式
-        ctx.setStrokeStyle('red')
-        ctx.setLineWidth(20)
-        //圆形端点
-        ctx.setLineCap('round')
-        //尖角连接
-        ctx.setLineJoin('miter')
-        ctx.moveTo(50,50)
-        ctx.lineTo(250,50)
-        ctx.lineTo(50,250)
-        ctx.lineTo(250,250)
-        ctx.stroke()
-        ctx.draw()
-        //恢复原来的样式
-        ctx.setLineWidth(1)
-        ctx.setLineCap('butt')
-        ctx.setLineJoin('mitter')
-    },
-    //字体绘制
-    drawText:function(){
-        ctx.setFillStyle("red")
-        ctx.setFontSize(40)//设置字体大小
-        ctx.setTextBaseline('bottom')
-        ctx.fillText('HDU杭电',80,80)//绘制文本
-
-        ctx.setFillStyle('yellow')
-        ctx.setTextBaseline('top')
-        ctx.fillText('Hello World',80,80)
-
-        ctx.setFillStyle('black')
-        ctx.rotate(30*Math.PI/180) //旋转字体
-        ctx.fillText("你好",150,80)
-        ctx.draw()
-    },
-    //绘制圆形渐变
-    circularGrad:function(){
-        var grd = ctx.createCircularGradient(175,175,125)//以175.175为圆心125为半径渐变
-        var sss = ctx.createLinearGradient(175, 175, 125) 
-        grd.addColorStop(0,'purple')//以渐变为起点
-        grd.addColorStop(1,'white')//添加渐变终点
-        ctx.setFillStyle(grd)
-        //起点50，50宽高都是250
-        ctx.fillRect(50,50,250,250)
-        ctx.draw()
-    },
-    //设置阴影矩形
-    shadowRect:function(){
-        ctx.setFillStyle('orange')
-        ctx.setShadow(20,20,50,'yellow')
-        ctx.fillRect(50,50,250,250)
-        ctx.draw()
-    },
-    //设置透明
-    translucent:function(){
-        ctx.setFillStyle('red')
-        ctx.setGlobalAlpha(0.2)//透明
-        ctx.fillRect(50,50,250,250)
-        ctx.draw()
-        ctx.setGlobalAlpha(1)//恢复设置
+    clearAll:function(){
+        this.setData({
+            pen:5,
+            color:"#000000"
+        })
+        this.ctx.draw();
+    
     }
 })
