341c7cc1e38e9b41a00e4e2147712e62826fbb28
whp <whp98@outlook.com>
2020-05-12 15:00:24 +0800

    完成动画操作

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,75 +1,80 @@
 //index.js
 //创建画布上下文
 Page({
-    //清除
-    isClear:false,
-    data:{
-        pen:5,
-        color:'#000000'
+    onReady:function(){
+        // 创建动画实例
+        this.animation = wx.createAnimation()
     },
-    onLoad:function(){
-        this.ctx =wx.createCanvasContext('myCanvas', this)//绘制画布绘图环境
-    },
-    touchStart:function(e){
-        this.x1 = e.changedTouches[0].x;
-        this.y1 = e.changedTouches[0].y;
-        if(this.isClear){
-            this.ctx.setStrokeStyle("#FFFFFF");
-            this.ctx.setLineCap("round");
-            this.ctx.setLineJoin('round');
-            this.ctx.setLineWidth(20);
-            this.ctx.beginPath();
-        }else{
-            this.ctx.setStrokeStyle(this.data.color);
-            this.ctx.setLineWidth(this.data.pen);
-            this.ctx.setLineCap('round');
-            this.ctx.beginPath();
-        }
-    },
-
-    touchMove:function(e){
-        var x2 = e.changedTouches[0].x;
-        var y2 = e.changedTouches[0].y;
-        if(this.isClear){
-            this.ctx.moveTo(this.x1,this.y1);
-            this.ctx.lineTo(x2,y2);
-            this.ctx.stroke();
-            this.x1=x2;
-            this.y1=y2;
-        }else{
-            this.ctx.moveTo(this.x1,this.y1);
-            this.ctx.lineTo(x2,y2);
-            this.ctx.stroke();
-            this.x1=x2;
-            this.y1=y2;
-        }
-        this.ctx.draw(true);
-    },
-    touchEnd:function(){},
-
-    penSelect:function(e){
-        console.log(222)
+    rotate:function(){
+        this.animation.rotate(Math.random()*720-360).step()
         this.setData({
-            pen:e.currentTarget.dataset.param
-        });
-        this.isClear = false;
-    },
-    colorSelect: function (e) {
-        this.setData({
-            color: e.currentTarget.dataset.param
-        });
-        this.isClear = false;
-    },
-    clear:function(){
-        this.isClear=true;
-    },
-
-    clearAll:function(){
-        this.setData({
-            pen:5,
-            color:"#000000"
+            animation:this.animation.export()
         })
-        this.ctx.draw();
-    
+    },
+    scale:function(){
+        // 缩放
+        this.animation.scale(Math.random()*2).step()
+        this.setData({
+            animation:this.animation.export()
+        })
+    },
+    translate:function(){
+        // 平移动画
+        this.animation.translate(50,50).step()
+        this.setData({
+            animation:this.animation.export()
+        });
+        
+    },
+    skew: function () {
+        // 倾斜动画
+        this.animation.skew(0,30).step()
+        this.setData({
+            animation:this.animation.export()
+        })
+    },
+    rotateAndScale:function(){
+        //旋转并缩放
+        this.animation.rotate(Math.random()*720-360).scale(Math.random()*2).step()
+        this.setData({
+            animation:this.animation.export()
+        })
+    },
+    // 旋转之后缩放
+    rotateThenScale:function(){
+        this.animation.rotate(Math.random()*720-360).step().scale(Math.random()*2).step()
+        this.setData({
+            animation:this.animation.export()
+        })
+    },
+    all:function(){
+        this.animation.rotate(Math.random*100 - 50,Math.random()*100 -50)
+        .scale(3)
+        .translate(30,30)
+        .skew(20,30)
+        .step()
+        this.setData({
+            animation:this.animation.export()
+        })
+    },
+    allInQueue:function(){
+        // 顺序显示所有动画
+        this.animation.rotate(Math.random * 100 - 50, Math.random() * 100 - 50)
+            .step().scale(3)
+            .step().translate(30, 30)
+            .step().skew(20, 30)
+            .step()
+    },
+    reset:function(){
+        this.animation.rotate(0,0)
+        .scale(1)
+        .translate(0,0)
+        .skew(0,0)
+        .step({
+            duration:0
+        })
+        this.setData({
+            animation:this.animation.export()
+        })
     }
 })
