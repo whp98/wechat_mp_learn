77d4e96ff1486bb85e5f4933ce25090d9ec65278
whp <whp98@outlook.com>
2020-06-02 14:31:12 +0800

    完成数据缓存的操作

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,85 +4,181 @@ var tempFilePaths,tempFilePath;
 Page({
     data:{
         msg:'',
-        hidden:true
+        hidden:true  //默认隐藏
     },
-    openFile:function(){
-        var that = this;
-        wx.chooseImage({
-            success(res) {
-                tempFilePaths = res.tempFilePaths
-                //console.log('打开文件路径'+tempFilePaths)
-                that.setData({
-                    imgPath:tempFilePaths[0],
-                    hidden:false,
-                    msg:'文件打开成功！'
-                })
-            },
-        })
+    Student:function(id,name,Chinese,Math,English){
+        // 一个学生对象包含学号姓名语文数学英语成绩
+        this.id = id;
+        this.name = name;
+        this.Chinese = Chinese;
+        this.Math = Math;
+        this.English = English;
     },
-    saveFile:function(){
+    loadStudents:function(){
+        var Student = new Array();//创建学生数组
+        var stu1 = new this.Student(1,'Tom',95,90,98);
+        var stu2 = new this.Student(2,'Has',67,72,80);
+        Student.push(stu1);
+        Student.push(stu2);
+        return Student; //返回学生数组
+    },
+    
+    setStorage:function(){ //定义异步存储数据的函数
         var that = this;
-        wx.saveFile({
-            tempFilePath:tempFilePaths[0],
-            success(res){
-                
-                console.log('保存文件路径'+res.savedFilePath);
+        wx.setStorage({ //调用异步存储函数
+            key: '高一',
+            data: this.loadStudents(),
+            success:function(){
                 that.setData({
-                    hidden:false,
-                    msg:'文件保存成功'
+                    hidden: false,//可见
+                    msg: "异步数据存储成功！"
                 })
             }
         })
     },
-    getSavedFileInfo:function(){
-        var i,file;
+    setStorageSync: function () { //同步存储数据
         var that = this;
-        wx.getSavedFileList({
+        wx.setStorageSync("高二", this.loadStudents());
+        that.setData({
+            hidden:false,
+            msg:"同步存储数据成功！"
+        })
+    },
+    //异步获取数据,哈哈
+    getStorage:function(){
+        var that = this;
+        wx.getStorage({
+            key:"高一",
             success:function(res){
-                if(res.fileList.length==0){
+                var length = res.data.length;
+                if(length>1){
                     that.setData({
                         hidden:false,
-                        msg:'没有文件信息'
+                        msg:"异步数据存储成功，学生1为："+
+                        "\n学号："+res.data[length-2].id+
+                        "\n姓名："+res.data[length-2].name+
+                        "\n语文成绩："+res.data[length-2].Chinese+
+                        "\n数学成绩:"+res.data[length-2].Math+
+                        "\n 英语成绩:"+res.data[length-2].English
                     })
-                }else{
-                    for(i=0;i<res.fileList.length;i++){
-                       file = res.fileList[i];
-                       console.log("第"+(i+1)+'个文件路径:'+file.filePath)
-                       wx.getSavedFileInfo({
-                           filePath: file.filePath,
-                           success:function(res){
-                               console.log('第'+i+'个文件大小为：'+res.size)
-                               that.setData({
-                                   hidden:false,
-                                   msg:'文件数量'+i+'\n最后一个文件的大小：'+res.size+'\n最后一个文件创建时间:' + res.createTime
-
-                               })
-                           }
-                       })
-                    }
+                    console.log(res.data)
                 }
-            }
-        })
-    },
-    removedSavedFile:function(){
-        var i,file;
-        var that = this;
-        // 获取已经保存的文件列表
-        wx.getSavedFileList({
-            success:function(res){
-                for(i=0;i<res.fileList.length;i++){
-                    file = res.fileList[i];
-                    wx.removeSavedFile({
-                        filePath: file.filePath,
-                    })
-                    console.log("第"+(i+1)+'个文件被删除')
-                }
+            },
+            fail:function(){
                 that.setData({
                     hidden:false,
-                    msg:'文件被删除'
+                    msg:"异步获取数据失败！"
                 })
             }
         })
+    },
+
+    // 同步获取数据
+    getStorageSync:function(){
+        var that = this;
+        // 避免意外
+        try{
+            var value = wx.getStorageSync("高二");
+            var length = value.length;
+            if(length>1){
+                that.setData({
+                    hidden:false,
+                    msg:'同步获取缓存数据成功，学生2为：'+
+                    "\n学号"+value[length-1].id+
+                    "\n姓名"+value[length-1].name+
+                    "\n语文成绩"+value[length-1].Chinese+
+                    "\n数学成绩"+value[length-1].Math+
+                    "\n英语成绩"+value[length-1].English
+                })
+                console.log(value)
+            }
+        }catch(e){
+            that.setData({
+                hidden:false,
+                msg:"同步数据获取失败！"
+            })
+            console.log(e);
+        }
+    },
+    // 获取有缓存信息的函数
+    getStorageInfo:function(){
+        var that = this;
+        wx.getStorageInfo({
+            success:function(res){
+                that.setData({
+                    hidden:false,
+                    msg:"异步信息获取成功\n"+
+                    "使用空间："+res.currentSize
+                    +"\n最大使用空间内为："+res.limitSize
+                })
+                console.log(res)
+            },
+            fail:function(){
+                that.setData({
+                    hidden:false,
+                    msg:'异步获取缓存信息失败！'
+                })
+            }
+        })
+    },
+
+    // 同步获取缓存信息
+    getStorageInfoSync:function(){
+        var that = this;
+        try{
+            var res = wx.getStorageInfoSync()
+            that.setData({
+                hidden:false,
+                msg:"同步获取缓存信息成功！\n"+
+                "已经使用空间为："+res.currentSize+
+                "\n最大空间为："+res.limitSize
+            })
+            console.log(res)
+        }catch(e){
+            that.setData({
+                hidden:false,
+                msg:"同步获取缓存信息失败"
+            })
+            console.log(e)
+        }
+    },
+    // 异步删除
+    removeStorage:function(){
+        var that = this;
+        wx.removeStorage({
+            key:'高一',
+            success:function(res){
+                that.setData({
+                    hidden:false,
+                    msg:"异步删除缓存数据成功！"
+                })
+                console.log(res.data)
+            },
+            fail:function(){
+                that.setData({
+                    hidden:false,
+                    msg:"异步删除数据失败"
+                })
+            }
+        })
+    },
+    // 同步删除数据
+    removeStorageSync:function(){
+        var that = this;
+        try{
+            // 同步删除数据
+            wx.removeStorageSync("高二");
+            that.setData({
+                hidden:false,
+                msg:"同步删除数据成功！"
+            })
+        }catch(e){
+            that.setData({
+                hidden:false,
+                msg:"同步删除缓存数据失败！"
+            })
+            console.log(e)
+        }
     }
-    
+
 })
