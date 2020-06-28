571b7ce515be2c091fe2e0dd5a2db4b5b2b8951d
whp <whp98@outlook.com>
2020-06-29 03:00:45 +0800

    同步和异步获取系统信息

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,50 +1,56 @@
-// pages/API/ScancodeAndMakephonecal/index.js
+// pages/API/DeviceInfo/index.js
 Page({
   data: {
-    brightness: '待查询',
-    copyBrightness: ''
+    hide1: false,
+    hide2: true
   },
-  setScreenBrightness: function (e) {
-    wx.setScreenBrightness({ //设置屏幕亮度API函数
-      value: e.detail.value //将slider组件的值传递给函数的参数
-    })
-  },
-  getScreenBrightness: function () {
+  getSystemInfo: function() { //异步获取设备信息
     var that = this;
-    wx.getScreenBrightness({ //获取屏幕亮度API函数
-      success: function (res) {
-        that.setData({ //将屏幕亮度值设置为保留小数点后一位并传递给绑定数据
-          brightness: res.value.toFixed(1)
+    wx.getSystemInfo({
+      success: (res) => {
+        that.setData({
+          msg: '异步',
+          hide1: false,
+          hide2: true,
+          model: res.model,
+          pixelRatio: res.pixelRatio,
+          screenWidth: res.screenWidth,
+          screenHeight: res.screenHeight,
+          windowWidth: res.windowWidth,
+          windowHeight: res.windowHeight,
+          language: res.language,
+          version: res.version,
+          system: res.system,
+          platform: res.platform,
+          SDKVersion: res.SDKVersion
         })
       },
     })
   },
-  setKeepScreenOn: function (e) {
-    let isKeeping = e.detail.value //将switch组件的值赋值给自定义变量
-    if (isKeeping) { //如果switch组件被选中，则设置屏幕保持亮度
-      wx.setKeepScreenOn({
-        keepScreenOn: true
+
+  getSystemInfoSync: function() {
+    var that = this;
+    try {
+      var res = wx.getSystemInfoSync();
+      that.setData({
+        msg: '同步',
+        hide1: true,
+        hide2: false,
+        model: res.model,
+        pixelRatio: res.pixelRatio,
+        screenWidth: res.screenWidth,
+        screenHeight: res.screenHeight,
+        windowWidth: res.windowWidth,
+        windowHeight: res.windowHeight,
+        language: res.language,
+        version: res.version,
+        system: res.system,
+        platform: res.platform,
+        SDKVersion: res.SDKVersion
       })
-      wx.vibrateShort() //手机短时振动
+    } catch (e) {
+      console.log(e)
     }
-  },
-  copyBrightness: function () {
-    var that = this
-    let brightness = this.data.brightness
-    wx.setClipboardData({ //设置剪贴板数据API函数
-      data: brightness, //给剪贴板数据赋值
-      success: function (res) {
-        wx.showToast({ //显示提示信息
-          title: '复制成功！'
-        })
-      }
-    })
-    wx.getClipboardData({ //获取剪贴板数据API函数 
-      success: function (res) {
-        that.setData({
-          copyBrightness: res.data //将剪贴板数据赋值给绑定变量
-        })
-      }
-    })
   }
+
 })
