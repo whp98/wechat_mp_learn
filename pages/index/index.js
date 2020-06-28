71b904d9546b4d30393e04a99432e489d656406f
whp <whp98@outlook.com>
2020-06-29 02:47:17 +0800

    屏幕亮度

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,45 +1,50 @@
 // pages/API/ScancodeAndMakephonecal/index.js
 Page({
-    name:'',    //定义联系人姓名
-    phone: '',  //定义联系人电话
-    scanCode: function() {
-      var that = this;
-      wx.scanCode({   //调用扫码API函数
-        onlyFromCamera: false,  //通过摄像头和调用相册图片都可以进行扫码
-        scanType: [], //不指定码的类型
-        success: function(res) {
-          that.setData({
-            resCode: res  //获取扫码结果
-          })
-        },
-      })
-    },
-    inputName: function(e) {
-      this.name = e.detail.value;  //获取联系人姓名
-    },
-    inputPhone: function(e) {
-      this.phone = e.detail.value;  //获取联系人电话
-    },
-    makeCall: function() {
-      let phone = this.phone;
-      wx.makePhoneCall({  //调用打电话API函数
-        phoneNumber: phone
-      })
-    },
-    addPerson: function() {
-      let name = this.name;
-      let phone = this.phone;
-      if (name == '' || phone == '') {
-        wx.showToast({
-          title: '姓名和电话不能为空',
-          icon: 'none',
-          duration: 2000
+  data: {
+    brightness: '待查询',
+    copyBrightness: ''
+  },
+  setScreenBrightness: function (e) {
+    wx.setScreenBrightness({ //设置屏幕亮度API函数
+      value: e.detail.value //将slider组件的值传递给函数的参数
+    })
+  },
+  getScreenBrightness: function () {
+    var that = this;
+    wx.getScreenBrightness({ //获取屏幕亮度API函数
+      success: function (res) {
+        that.setData({ //将屏幕亮度值设置为保留小数点后一位并传递给绑定数据
+          brightness: res.value.toFixed(1)
         })
-      } else {
-        wx.addPhoneContact({  //调用添加联系人API函数
-          firstName: name,
-          mobilePhoneNumber: phone
+      },
+    })
+  },
+  setKeepScreenOn: function (e) {
+    let isKeeping = e.detail.value //将switch组件的值赋值给自定义变量
+    if (isKeeping) { //如果switch组件被选中，则设置屏幕保持亮度
+      wx.setKeepScreenOn({
+        keepScreenOn: true
+      })
+      wx.vibrateShort() //手机短时振动
+    }
+  },
+  copyBrightness: function () {
+    var that = this
+    let brightness = this.data.brightness
+    wx.setClipboardData({ //设置剪贴板数据API函数
+      data: brightness, //给剪贴板数据赋值
+      success: function (res) {
+        wx.showToast({ //显示提示信息
+          title: '复制成功！'
         })
       }
-    }
-  })
+    })
+    wx.getClipboardData({ //获取剪贴板数据API函数 
+      success: function (res) {
+        that.setData({
+          copyBrightness: res.data //将剪贴板数据赋值给绑定变量
+        })
+      }
+    })
+  }
+})
