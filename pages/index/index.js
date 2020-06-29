f91addec8dc673689788cd77ab5b0f9e09b04cf7
whp <whp98@outlook.com>
2020-06-29 14:37:37 +0800

    完成菜单操作

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,103 +1,25 @@
-// pages/index/index.js
+// pages/API/ActionSheet/index.js
+var myItemList = ['第一项', '第二项', '第三项', '第四项']
 Page({
-  showTabBar:function(){
-    wx.showTabBar({
-      aniamtion: true,
-      success: function(res) {},
-      fail: function(res) {},
+  showActionSheet: function() {
+    var that = this;
+    wx.showActionSheet({ //调用API函数显示操作菜单
+      itemList: myItemList, //操作菜单项列表
+      itemColor: '#0000FF', //操作菜单项文字颜色
+      success: function(res) {
+        console.log(myItemList)
+        that.setData({
+          tapIndex: res.tapIndex, //点击的菜单序号
+          tapItem: myItemList[res.tapIndex] //点击的菜单项
+        })
+      },
+      fail: function(res) {
+        that.setData({
+          tapIndex: -1,
+          tapItem: '取消'
+        })
+      },
       complete: function(res) {},
     })
-  },
-
-  hideTabBar:function(){
-    wx.hideTabBar({
-      aniamtion: true,
-      success: function(res) {},
-      fail: function(res) {},
-      complete: function(res) {},
-    })
-  },
-
-  setTabBarBadge:function(){
-    wx.setTabBarBadge({
-      index: 3,
-      text: '10',
-      success: function(res) {},
-      fail: function(res) {},
-      complete: function(res) {},
-    })
-  },
-
-  removeTabBarBadge:function(){
-    wx.removeTabBarBadge({
-      index: 3,
-      success: function(res) {},
-      fail: function(res) {},
-      complete: function(res) {},
-    })
-  },
-
-  showTabBarRedDot:function(){
-    wx.showTabBarRedDot({
-      index: 1,
-      success: function(res) {},
-      fail: function(res) {},
-      complete: function(res) {},
-    })
-  },
-
-  hideTabBarRedDot:function(){
-    wx.hideTabBarRedDot({
-      index: 1,
-      success: function(res) {},
-      fail: function(res) {},
-      complete: function(res) {},
-    })
-  },
-
-  setTabBarStyle:function(){
-    wx.setTabBarStyle({
-      color: '#ff0000',
-      selectedColor: '#0000ff',
-      backgroundColor: '#ffff00',
-      borderStyle: '',
-      success: function(res) {},
-      fail: function(res) {},
-      complete: function(res) {},
-    })
-  },
-
-  setTabBarItem:function(){
-    wx.setTabBarItem({
-      index: 4,
-      text: '云开发',
-      iconPath: '/images/cloud.png',
-      selectedIconPath: '/images/cloud-selected.png',
-      success: function(res) {},
-      fail: function(res) {},
-      complete: function(res) {},
-    })
-  },
-
-  reset:function(){
-    wx.setTabBarStyle({
-      color: '#000000',
-      selectedColor: '#00ff00',
-      backgroundColor: '#fff',
-      borderStyle: '',
-      success: function(res) {},
-      fail: function(res) {},
-      complete: function(res) {},
-    })
-    wx.setTabBarItem({
-      index: 4,
-      text: '关于我们',
-      iconPath: '/images/guanyu-off.png',
-      selectedIconPath: '/images/guanyu-on.png',
-      success: function(res) {},
-      fail: function(res) {},
-      complete: function(res) {},
-    })
-  },
-
+  }
 })
